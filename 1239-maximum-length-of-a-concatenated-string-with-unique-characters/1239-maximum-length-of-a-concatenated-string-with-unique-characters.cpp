db34c2aec08b25eb03852640ca03edd70c8902da
Vishvam Pawar <90624208+vishvampawar11@users.noreply.github.com>
2022-10-24 09:45:14 +0530

    Time: 8 ms (90.10%), Space: 8.2 MB (86.03%) - LeetHub

---
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int solve(int i, int msk, int n, vector<int> &masks) {
+        if(i==n) 
+            return __builtin_popcount(msk);
+        if((masks[i] & msk) == 0)
+            return max(solve(i+1, (masks[i] | msk), n, masks), solve(i+1, msk, n, masks));
+        else
+            return solve(i+1, msk, n, masks);
+    }
+    
+    int maxLength(vector<string>& arr) {
+        vector<int> masks;
+        int i = 0;
+        while(i<arr.size()) {
+            vector<int> cnt(26);
+            bool flag = false;
+            for(int j=0;j<arr[i].size();j++) {
+                cnt[arr[i][j]-'a']++;
+                if(cnt[arr[i][j]-'a'] > 1) {
+                    flag = true;
+                    break;
+                }
+            }
+            if(flag) {
+                arr.erase(arr.begin()+i);
+            }
+            else
+                i++;
+        }
+        for(int i=0;i<arr.size();i++) {
+            int msk = 0;
+            for(int j=0;j<arr[i].size();j++)
+                msk |= (1 << (arr[i][j] - 'a'));
+            masks.push_back(msk);
+        }
+        return solve(0, 0, arr.size(), masks);
+    }
+};
