75beec6ef0979d3b3ea9c4b1b562e978963c9e52
Vishvam Pawar <90624208+vishvampawar11@users.noreply.github.com>
2022-10-24 09:49:29 +0530

    Time: 3 ms (98.05%), Space: 8.3 MB (84.44%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
+    
+    // recursive function to check all possible subsequences
     int solve(int i, int msk, int n, vector<int> &masks) {
         if(i==n) 
             return __builtin_popcount(msk);
+        
+        // if two strings doesn't have common elements we have a choice to select ith string
         if((masks[i] & msk) == 0)
             return max(solve(i+1, (masks[i] | msk), n, masks), solve(i+1, msk, n, masks));
+        // if they have common elements we can't select ith string
         else
             return solve(i+1, msk, n, masks);
     }
     
-    int maxLength(vector<string>& arr) {
-        vector<int> masks;
+    int maxLength(vector<string>& arr) {       
+        // removing all strings which contain duplicates
         int i = 0;
         while(i<arr.size()) {
             vector<int> cnt(26);
@@ -28,12 +33,16 @@ public:
             else
                 i++;
         }
+        
+        // forming bitmasks for all strings
+        vector<int> masks;
         for(int i=0;i<arr.size();i++) {
             int msk = 0;
             for(int j=0;j<arr[i].size();j++)
                 msk |= (1 << (arr[i][j] - 'a'));
             masks.push_back(msk);
         }
+        
         return solve(0, 0, arr.size(), masks);
     }
 };
