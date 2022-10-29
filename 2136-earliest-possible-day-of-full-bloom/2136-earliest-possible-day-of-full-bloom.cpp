3e4c735f10a8bd065bb48de61a84fba8aeb91bf0
Vishvam Pawar <90624208+vishvampawar11@users.noreply.github.com>
2022-10-29 18:52:52 +0530

    Time: 1793 ms (5.02%), Space: 231.9 MB (5.82%) - LeetHub

---
+++ 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int earliestFullBloom(vector<int>& pt, vector<int>& gt) {
+        int n = pt.size();
+        vector<vector<int>> v;
+        for(int i=0;i<n;i++) 
+            v.push_back({gt[i],pt[i]});
+        sort(v.begin(),v.end(), greater<vector<int>>());
+        int days = 0;
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            days+=v[i][1];
+            ans=max(ans,(days + v[i][0]));
+        }
+        return ans;
+    }
+};
